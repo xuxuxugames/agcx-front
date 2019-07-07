80f546c8b79f2bebb5eae340155f46bd3caf279b
kotoyuuko <kokoro@core.moe>
2019-07-07 19:54:09 +0800

    work with vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,16 @@
 <template>
   <div id="app">
-    <TwentyFourtyEight
-      ref="tfe"
-      :onScore="updateScore"
-      :onFinish="finishGame"
-    />
-    <p>{{ score }}</p>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/2048">2048</router-link>
+    </div>
+    <router-view />
   </div>
 </template>
 
 <script>
-import TwentyFourtyEight from './games/twenty-fourty-eight'
-
 export default {
-  name: 'app',
-  components: {
-    TwentyFourtyEight
-  },
-  data () {
-    return {
-      score: 0
-    }
-  },
-  methods: {
-    updateScore (score) {
-      this.score = score
-    },
-    finishGame (status) {
-      console.log(this.score, status)
-    }
-  }
+  name: 'app'
 }
 </script>
 
---
+++ src/views/Game2048.vue
@@ -0,0 +1,37 @@
+<template>
+  <section>
+    <TwentyFourtyEight
+      ref="tfe"
+      :onScore="updateScore"
+      :onFinish="finishGame"
+    />
+    <p>{{ score }}</p>
+  </section>
+</template>
+
+<script>
+import TwentyFourtyEight from '@/games/twenty-fourty-eight'
+
+export default {
+  name: 'Game2048',
+  components: {
+    TwentyFourtyEight
+  },
+  data () {
+    return {
+      score: 0
+    }
+  },
+  methods: {
+    updateScore (score) {
+      this.score = score
+    },
+    finishGame (status) {
+      console.log(this.score, status)
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+</style>

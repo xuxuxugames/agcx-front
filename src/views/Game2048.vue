8483deae0d1c8900d148e0966fec3640b8de6a3d
kotoyuuko <kokoro@core.moe>
2019-07-08 10:58:40 +0800

    arrow keys in game page

--- src/views/Game2048.vue
+++ src/views/Game2048.vue
@@ -1,11 +1,20 @@
 <template>
-  <section>
+  <section id="game">
     <TwentyFourtyEight
       ref="tfe"
       :onScore="updateScore"
       :onFinish="finishGame"
     />
-    <p>{{ score }}</p>
+    <div class="controller">
+      <div class="score">{{ score }}</div>
+      <div class="tf-ctrl">
+        <div class="arrow up"><i class="el-icon-arrow-up"></i></div>
+        <div class="arrow left"><i class="el-icon-arrow-left"></i></div>
+        <div class="arrow center"></div>
+        <div class="arrow right"><i class="el-icon-arrow-right"></i></div>
+        <div class="arrow down"><i class="el-icon-arrow-down"></i></div>
+      </div>
+    </div>
   </section>
 </template>
 
@@ -33,5 +42,79 @@ export default {
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scope>
+#game {
+  display: flex;
+  flex-wrap: wrap;
+
+  .controller {
+    margin-left: 20px;
+    width: 250px;
+
+    .tf-ctrl {
+      margin-top: 120px;
+      margin-left: 50px;
+
+      .arrow {
+        margin: 0;
+        width: 50px;
+        height: 45px;
+        border: 1px solid #bdc0ba;
+        border-radius: 5px;
+        transition: 100ms;
+        position: relative;
+        font-size: 30px;
+        text-align: center;
+        padding-top: 5px;
+      }
+
+      .arrow:hover {
+        box-shadow: 0 0 10px #787d7b;
+      }
+
+      .arrow.center {
+        border: 0;
+      }
+
+      .arrow.center:hover {
+        box-shadow: none;
+      }
+
+      .arrow.up {
+        left: 55px;
+      }
+
+      .arrow.left {
+        top: 5px;
+        left: 0;
+      }
+
+      .arrow.center {
+        top: -45px;
+        left: 55px;
+      }
+
+      .arrow.right {
+        top: -98px;
+        left: 110px;
+      }
+
+      .arrow.down {
+        top: -92px;
+        left: 55px;
+      }
+    }
+
+    .score {
+      float: right;
+      width: 150px;
+      height: 40px;
+      border: 1px solid #bdc0ba;
+      border-radius: 5px;
+      font-size: 20px;
+      text-align: center;
+      padding-top: 10px;
+    }
+  }
+}
 </style>

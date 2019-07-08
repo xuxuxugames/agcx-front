3c2be29464efa2e0de2850f648742572e57982b2
kotoyuuko <kokoro@core.moe>
2019-07-08 15:55:18 +0800

    fix style for table on other pages

--- src/games/twenty-fourty-eight/index.vue
+++ src/games/twenty-fourty-eight/index.vue
@@ -40,232 +40,232 @@ export default {
   font-family: "Clear Sans", sans-serif;
   font-size: 21px;
   width: 450px;
-}
 
-.text {
-  order: 2;
-  padding-top: 40px;
-  width: 440px;
-  font-weight: bold;
-}
+  .text {
+    order: 2;
+    padding-top: 40px;
+    width: 440px;
+    font-weight: bold;
+  }
 
-.board {
-  order: 1;
-  width: 440px;
-  height: 440px;
-  padding: 5px;
-  background-color: #baa;
-  border-radius: 7px;
-  outline: none;
-  position: relative;
-}
+  .board {
+    order: 1;
+    width: 440px;
+    height: 440px;
+    padding: 5px;
+    background-color: #baa;
+    border-radius: 7px;
+    outline: none;
+    position: relative;
+  }
 
-.board .cell,
-.tile {
-  user-select: none;
-  cursor: default;
-}
+  .board .cell,
+  .tile {
+    user-select: none;
+    cursor: default;
+  }
 
-.cell,
-.tile {
-  width: 100px;
-  height: 100px;
-  margin: 5px;
-  line-height: 90px;
-  display: inline-block;
-  font-size: 55px;
-  font-weight: bold;
-  text-align: center;
-  vertical-align: middle;
-  border-radius: 7px;
-  font-family: "Clear Sans";
-  color: #766;
-  background-color: #dcb;
-}
+  .cell,
+  .tile {
+    width: 100px;
+    height: 100px;
+    margin: 5px;
+    line-height: 90px;
+    display: inline-block;
+    font-size: 55px;
+    font-weight: bold;
+    text-align: center;
+    vertical-align: middle;
+    border-radius: 7px;
+    font-family: "Clear Sans";
+    color: #766;
+    background-color: #dcb;
+  }
 
-.tile0 {
-  background-color: #dcb;
-}
+  .tile0 {
+    background-color: #dcb;
+  }
 
-.tile2 {
-  background-color: #eee;
-}
+  .tile2 {
+    background-color: #eee;
+  }
 
-.tile4 {
-  background-color: #eec;
-}
+  .tile4 {
+    background-color: #eec;
+  }
 
-.tile8 {
-  color: #ffe;
-  background-color: #fb8;
-}
+  .tile8 {
+    color: #ffe;
+    background-color: #fb8;
+  }
 
-.tile16 {
-  color: #ffe;
-  background-color: #f96;
-}
+  .tile16 {
+    color: #ffe;
+    background-color: #f96;
+  }
 
-.tile32 {
-  color: #ffe;
-  background-color: #f75;
-}
+  .tile32 {
+    color: #ffe;
+    background-color: #f75;
+  }
 
-.tile64 {
-  color: #ffe;
-  background-color: #f53;
-}
+  .tile64 {
+    color: #ffe;
+    background-color: #f53;
+  }
 
-.tile128 {
-  color: #ffe;
-  background-color: #ec7;
-  font-size: 45px;
-}
+  .tile128 {
+    color: #ffe;
+    background-color: #ec7;
+    font-size: 45px;
+  }
 
-.tile256 {
-  color: #ffe;
-  background-color: #ec6;
-  font-size: 45px;
-}
+  .tile256 {
+    color: #ffe;
+    background-color: #ec6;
+    font-size: 45px;
+  }
 
-.tile512 {
-  color: #ffe;
-  background-color: #ec5;
-  font-size: 45px;
-}
+  .tile512 {
+    color: #ffe;
+    background-color: #ec5;
+    font-size: 45px;
+  }
 
-.tile1024 {
-  color: #fff;
-  background-color: #ec3;
-  font-size: 35px;
-}
+  .tile1024 {
+    color: #fff;
+    background-color: #ec3;
+    font-size: 35px;
+  }
 
-.tile2048 {
-  color: #fff;
-  background-color: #ec2;
-  font-size: 35px;
-}
+  .tile2048 {
+    color: #fff;
+    background-color: #ec2;
+    font-size: 35px;
+  }
 
-.tile {
-  position: absolute;
-}
+  .tile {
+    position: absolute;
+  }
 
-.tile.merged {
-  display: none;
-}
+  .tile.merged {
+    display: none;
+  }
 
-.tile.merged.isMoving {
-  display: inline;
-}
+  .tile.merged.isMoving {
+    display: inline;
+  }
 
-.tile.new,
-.overlay {
-  animation-duration: 0.2s;
-  animation-name: newTile;
-  animation-fill-mode: forwards;
-  animation-delay: 0.15s;
-  transform: scale(0);
-}
-
-@keyframes newTile {
-  from {
+  .tile.new,
+  .overlay {
+    animation-duration: 0.2s;
+    animation-name: newTile;
+    animation-fill-mode: forwards;
+    animation-delay: 0.15s;
     transform: scale(0);
   }
 
-  to {
-    transform: scale(1);
-  }
-}
+  @keyframes newTile {
+    from {
+      transform: scale(0);
+    }
 
-.overlay {
-  position: absolute;
-  top: 0px;
-  bottom: 0px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  text-align: center;
-  left: 0px;
-  right: 0px;
-  font-size: 55px;
-  font-weight: bolder;
-  background-color: rgba(221, 221, 221, 0.5);
-  border-radius: 7px;
-}
-
-.tryAgain {
-  background-color: #876;
-  color: #fff;
-  height: 40px;
-  width: 200px;
-  font-size: 20px;
-  font-weight: bold;
-  cursor: pointer;
-  border: none;
-  border-radius: 5px;
-}
-
-.overlay .message {
-  color: #666;
-}
-
-@for $row from 0 through 3 {
-  @for $column from 0 through 3 {
-    .position_#{$row}_#{$column}:not(.isMoving) {
-      top: 110px * $row + 5px;
-      left: 110px * $column + 5px;
+    to {
+      transform: scale(1);
     }
   }
-}
 
-@for $fromRow from 0 through 3 {
-  @for $toRow from 0 through 3 {
-    $name: row_from_#{$fromRow}_to_#{$toRow};
+  .overlay {
+    position: absolute;
+    top: 0px;
+    bottom: 0px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    left: 0px;
+    right: 0px;
+    font-size: 55px;
+    font-weight: bolder;
+    background-color: rgba(221, 221, 221, 0.5);
+    border-radius: 7px;
+  }
 
-    @if $fromRow == $toRow {
-      .#{$name} {
-        top: 110px * $toRow + 5px;
+  .tryAgain {
+    background-color: #876;
+    color: #fff;
+    height: 40px;
+    width: 200px;
+    font-size: 20px;
+    font-weight: bold;
+    cursor: pointer;
+    border: none;
+    border-radius: 5px;
+  }
+
+  .overlay .message {
+    color: #666;
+  }
+
+  @for $row from 0 through 3 {
+    @for $column from 0 through 3 {
+      .position_#{$row}_#{$column}:not(.isMoving) {
+        top: 110px * $row + 5px;
+        left: 110px * $column + 5px;
       }
-    } @else {
-      .#{$name} {
-        animation-duration: 0.2s;
-        animation-name: $name;
-        animation-fill-mode: forwards;
-      }
+    }
+  }
 
-      @keyframes #{$name} {
-        from {
-          top: 110px * $fromRow + 5px;
-        }
-        to {
+  @for $fromRow from 0 through 3 {
+    @for $toRow from 0 through 3 {
+      $name: row_from_#{$fromRow}_to_#{$toRow};
+
+      @if $fromRow == $toRow {
+        .#{$name} {
           top: 110px * $toRow + 5px;
         }
+      } @else {
+        .#{$name} {
+          animation-duration: 0.2s;
+          animation-name: $name;
+          animation-fill-mode: forwards;
+        }
+
+        @keyframes #{$name} {
+          from {
+            top: 110px * $fromRow + 5px;
+          }
+          to {
+            top: 110px * $toRow + 5px;
+          }
+        }
       }
     }
   }
-}
 
-@for $fromColumn from 0 through 3 {
-  @for $toColumn from 0 through 3 {
-    $name: column_from_#{$fromColumn}_to_#{$toColumn};
+  @for $fromColumn from 0 through 3 {
+    @for $toColumn from 0 through 3 {
+      $name: column_from_#{$fromColumn}_to_#{$toColumn};
 
-    @if $fromColumn == $toColumn {
-      .#{$name} {
-        left: 110px * $toColumn + 5px;
-      }
-    } @else {
-      .#{$name} {
-        animation-duration: 0.2s;
-        animation-name: $name;
-        animation-fill-mode: forwards;
-      }
-
-      @keyframes #{$name} {
-        from {
-          left: 110px * $fromColumn + 5px;
-        }
-        to {
+      @if $fromColumn == $toColumn {
+        .#{$name} {
           left: 110px * $toColumn + 5px;
+        }
+      } @else {
+        .#{$name} {
+          animation-duration: 0.2s;
+          animation-name: $name;
+          animation-fill-mode: forwards;
+        }
+
+        @keyframes #{$name} {
+          from {
+            left: 110px * $fromColumn + 5px;
+          }
+          to {
+            left: 110px * $toColumn + 5px;
+          }
         }
       }
     }
